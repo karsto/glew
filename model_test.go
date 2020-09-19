307c092b1124968939b799a0910e5b7a6372bfb0
Ashton Kinslow <github@ashtonkinslow.com>
2020-09-19 12:47:21 -0500

    add ast test playground with poc

--- model_test.go
+++ model_test.go
@@ -1,6 +1,11 @@
 package glew
 
 import (
+	"fmt"
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"strings"
 	"testing"
 	"time"
 )
@@ -72,4 +77,62 @@ func TestPlayground(t *testing.T) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+var (
+	todoTokens = []string{"TODO", "FIXME"}
+)
+
+func isTodo(s string) (int, bool) {
+	for _, indent := range todoTokens {
+		if strings.HasPrefix(strings.ToUpper(s), indent) {
+			return len(indent), true
+		}
+	}
+	return 0, false
+}
+
+func TestParsePlayground(t *testing.T) {
+	fset := token.NewFileSet()
+	target, err := parser.ParseFile(fset, "./test-target.go", nil, parser.ParseComments)
+	if err != nil {
+		panic(err)
+	}
+
+	// ******* Comment Finder
+	//TODO: convert to util function to identify all todo places that need dev intervention in final program
+	for _, cg := range target.Comments {
+		for _, c := range cg.List {
+			for i, l := range strings.Split(c.Text, "\n") {
+				var t = strings.TrimSpace(l)
+				if strings.HasPrefix(t, "//") || strings.HasPrefix(t, "/*") || strings.HasPrefix(t, "*/") {
+					t = strings.TrimSpace(t[2:])
+				}
+
+				// To do found
+				if _, found := isTodo(t); found {
+					fmt.Printf("FOUND TODO on line: %v", fset.Position(c.Slash).Line+i)
+				}
+			}
+		}
+	}
+	// ******** End Comment Finder
+
+	// ******** Struct finder
+	ast.Inspect(target, func(n ast.Node) bool {
+		// TODO: extract meta info and convert to ModelMeta
+		// TODO: consider using ast field types ?
+		t, ok2 := n.(*ast.TypeSpec)
+		if ok2 {
+			fmt.Printf("Name:%v \n", t.Name.Name)
+		}
+		structBlock, ok := n.(*ast.StructType)
+		if ok {
+			for _, field := range structBlock.Fields.List {
+				fmt.Printf("Field: %s\n", field.Names[0].Name)
+				fmt.Printf("Tag:   %s\n", field.Tag.Value)
+			}
+		}
+		return true
+	})
 }
---
+++ test-target.go
@@ -0,0 +1,27 @@
+package glew
+
+import "time"
+
+// test comment
+// TODO: testing
+type TestModel2 struct {
+	StringField string    `db:"string_field" json:"stringField"`
+	NumField    int       `db:"num_field" json:"numField"`
+	ID          int       `db:"id" json:"id"`
+	Code        string    `db:"code" json:"code"`
+	TimeField   time.Time `db:"time_field" json:"timeField"`
+}
+type CreateTestModel2 struct {
+	StringField string    `db:"string_field" json:"stringField"`
+	NumField    int       `db:"num_field" json:"numField"`
+	ID          int       `db:"id" json:"id"`
+	Code        string    `db:"code" json:"code"`
+	TimeField   time.Time `db:"time_field" json:"timeField"`
+}
+type UpdateTestModel2 struct {
+	StringField string    `db:"string_field" json:"stringField"`
+	NumField    int       `db:"num_field" json:"numField"`
+	ID          int       `db:"id" json:"id"`
+	Code        string    `db:"code" json:"code"`
+	TimeField   time.Time `db:"time_field" json:"timeField"`
+}
